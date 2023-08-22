6b367dca7dc5427952d7581991b60a99756deef0
Kulpot <kulpotwahu@gmail.com>
2023-08-22 19:52:02 +0800

    Add project files.

---
+++ Basic Stack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=WOlrjNvM4jg&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=3
+//  about memory -- stack --
+
+class MainClass
+{
+    static void Goo()
+    {
+        int gooeyInt = 29;
+        double gooeyDouble = 43;
+    }
+    static void Main()
+    {
+        int myInt = 5;
+        int anotherInt = 10;
+        Goo();
+        double meDouble = 98.3;
+        Goo();
+    }
+}
